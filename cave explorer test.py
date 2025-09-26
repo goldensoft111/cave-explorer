264d92be2775d9ba0b3ed3212c36217d2adaffb7
goldensoft111 <holdenlaky@gmail.com>
2025-09-26 17:43:52 -0500

    Add files via upload

---
+++ cave explorer test.py
@@ -0,0 +1,15 @@
+import random
+allowed_numbers[1, 5, 10, 15, 20, 25, 30, 40, 60, 80, 99, 100]
+print("cave adventure")
+print("enter a username.")
+usr = input("name")
+print(usr+"?")
+ask = input("y for yes n for no.")
+if ask == "n":
+ print("type a new username")
+ usr = input("name")
+if ask == "y":
+     print("cave explorer")
+print('you  are in a cave.you can go straight, left, or right.')
+answer = input()
+print(answer)
